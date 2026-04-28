b22f055344377ff2e61eae4b2b755b4fde3e1364
LuisB <arvr@fusingeducation.com>
2026-04-28 09:25:35 -0500

    Update

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,6 @@
-const cacheName = "PC-WebAR_ITWT-0.1.0";
-const contentToCache = [
-    "Build/WebAR_Fusing.loader.js",
-    "Build/WebAR_Fusing.framework.js",
-    "Build/WebAR_Fusing.data",
-    "Build/WebAR_Fusing.wasm",
-    "TemplateData/style.css"
-
-];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
